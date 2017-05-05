a2f85b64f52c53fe83fb2ebd8f580b570e61320f
Mahima Singh <maheema268@gmail.com>
2017-05-04 23:56:08 -0400

    Add files via upload

---
+++ scripts/tabletop-feed.js
@@ -0,0 +1,67 @@
+var jqueryNoConflict = jQuery;
+
+// begin main function
+jqueryNoConflict(document).ready(function(){
+
+    initializeTabletopObject("1OUSZdjfcj1lkGrz9_Shh0mr3MRTyFTlrvqGIz0UtKGM");
+
+});
+
+// pull data from google spreadsheet
+function initializeTabletopObject(dataSpreadsheet){
+    Tabletop.init({
+        key: dataSpreadsheet,
+        callback: writeTableWith,
+        simpleSheet: true,
+        debug: false
+    });
+}
+
+// create table headers
+function createTableColumns(){
+
+    /* swap out the properties of mDataProp & sTitle to reflect
+    the names of columns or keys you want to display.
+    Remember, tabletop.js strips out spaces from column titles, which
+    is what happens with the More Info column header */
+
+    var tableColumns =   [
+		{"mDataProp": "link", "sTitle": "Link", "sClass": "center"},
+		{"mDataProp": "tag", "sTitle": "Tag", "sClass": "center"},
+		{"mDataProp": "source", "sTitle": "source", "sClass": "center"},
+        {"mDataProp": "time", "sTitle": "Time", "sClass": "center"}
+	];
+    return tableColumns;
+}
+
+// create the table container and object
+function writeTableWith(dataSource){
+
+    jqueryNoConflict("#demo").html("<table cellpadding='0' cellspacing='0' border='0' class='display table table-bordered table-striped' id='data-table-container'></table>");
+
+    var oTable = jqueryNoConflict("#data-table-container").dataTable({
+        // "sPaginationType": "bootstrap",
+        "iDisplayLength": 25,
+        "aaData": dataSource,
+        "aoColumns": createTableColumns(),
+        "fnRowCallback": function(nRow, aData, iDisplayIndex) {
+            console.log(aData);
+            $("td:eq(0)", nRow).html("<a href='" + aData.link + "'>Article</a>");
+            
+            return nRow;
+        },
+        "oLanguage": {
+            "sLengthMenu": "_MENU_ records per page"
+        }
+    });
+
+};
+
+//define two custom functions (asc and desc) for string sorting
+jQuery.fn.dataTableExt.oSort["string-case-asc"]  = function(x,y) {
+	return ((x < y) ? -1 : ((x > y) ?  0 : 0));
+};
+
+jQuery.fn.dataTableExt.oSort["string-case-desc"] = function(x,y) {
+	return ((x < y) ?  1 : ((x > y) ? -1 : 0));
+};
